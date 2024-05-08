f7fbb9a31962193a83d843c384f86a09aeb72690
alishashamoun <alishashamoun12345@gmail.com>
2024-05-08 15:15:46 +0500

    admin translate

---
+++ lang/en/admin/estimatereq/index.php
@@ -0,0 +1,36 @@
+<?php
+    return [
+
+        'estimate_request' => 'Estimate Request',
+        'create_estimate_request' => 'Create Estimate Request',
+
+
+    's_n' => 'S N',
+    'request' => 'Request',
+    'name' => 'Name',
+    'phone' => 'Phone',
+    'email' => 'Email',
+    'created_at' => 'Created At',
+    'action' => 'Action',
+    'estimate_request' => 'Estimate Request',
+    'home' => 'Home',
+    'created_by' => 'Created By',
+    'show' => 'Show',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'phone_number' => 'Phone Number',
+    'street_address' => 'Street Address',
+    'city' => 'City',
+    'state' => 'State',
+    'zip_code' => 'Zip Code',
+    'details' => 'Details',
+    'back_to_users' => 'Back To Users',
+    'edit_estimate_request' => 'Edit Estimate Request',
+    'picture(if_any)' => 'Picture(if any)',
+    'update_request' => 'Update Request',
+    'please_enter_details_of_requested_work_and/or_description_of_problem' => 'Please Enter Details of Requested Work and/or Description of Problem',
+
+    ];
+?>
--- resources/views/admin/estimate_req/show.blade.php
+++ resources/views/admin/estimate_req/show.blade.php
@@ -9,12 +9,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Estimate Request</h1>
+                        <h1>{{ __('admin/estimatereq/index.estimate_request') }}</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Estimate Request</li>
+                            <li class="breadcrumb-item active">{{ __('admin/estimatereq/index.estimate_request') }}</li>
                         </ol>
                     </div>
                 </div>
@@ -29,39 +29,39 @@
                         <table class="table">
                             <tbody>
                                 <tr>
-                                    <th>First Name:</th>
+                                    <th>{{ __('admin/estimatereq/index.first_name') }}:</th>
                                     <td>{{ $estimate->first_name }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Last Name:</th>
+                                    <th>{{ __('admin/estimatereq/index.last_name') }}:</th>
                                     <td>{{ $estimate->last_name }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Phone Number:</th>
+                                    <th>{{ __('admin/estimatereq/index.phone_number') }}:</th>
                                     <td>{{ $estimate->phone_number }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Email:</th>
+                                    <th>{{ __('admin/estimatereq/index.email') }}:</th>
                                     <td>{{ $estimate->email }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Street Address:</th>
+                                    <th>{{ __('admin/estimatereq/index.street_address') }}:</th>
                                     <td>{{ $estimate->street_address }}</td>
                                 </tr>
                                 <tr>
-                                    <th>City:</th>
+                                    <th>{{ __('admin/estimatereq/index.city') }}:</th>
                                     <td>{{ $estimate->city }}</td>
                                 </tr>
                                 <tr>
-                                    <th>State:</th>
+                                    <th>{{ __('admin/estimatereq/index.state') }}State:</th>
                                     <td>{{ $estimate->state }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Zip Code:</th>
+                                    <th>{{ __('admin/estimatereq/index.zip_code') }}:</th>
                                     <td>{{ $estimate->zip_code }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Details:</th>
+                                    <th>{{ __('admin/estimatereq/index.details') }}:</th>
                                     <td>{{ $estimate->details }}</td>
                                 </tr>
                                 @if ($estimate->picture)
@@ -75,7 +75,7 @@
                             </tbody>
                         </table>
 
-                        <a href="{{ route('estimate_requests.index') }}" class="btn btn-primary">Back to Users</a>
+                        <a href="{{ route('estimate_requests.index') }}" class="btn btn-primary">{{ __('admin/estimatereq/index.back_to_users') }}</a>
                     </div>
                 </div>
                 <!-- /.row -->
