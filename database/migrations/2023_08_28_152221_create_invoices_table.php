369fba58eba68fb388a7a7142a32e1b9652458fb
alishashamoun <alishashamoun12345@gmail.com>
2024-05-20 18:25:51 +0500

    column migration

--- database/migrations/2023_08_28_152221_create_invoices_table.php
+++ database/migrations/2023_08_28_152221_create_invoices_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             $table->string('note_to_cust');
             $table->string('no_bill_amount_description');
             $table->string('no_bill_amount');
+            $table->string('note_to_cust');
             $table->timestamps();
         });
     }
