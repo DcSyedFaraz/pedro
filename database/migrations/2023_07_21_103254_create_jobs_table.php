4540cb902c80a3f3e33f98baf2b378755f190c8c
alishashamoun <alishashamoun12345@gmail.com>
2024-05-21 17:38:51 +0500

    migration

--- database/migrations/2023_07_21_103254_create_jobs_table.php
+++ database/migrations/2023_07_21_103254_create_jobs_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->integer('customer_id')->nullable();
+            $table->integer('estimate_id')->nullable();
             $table->integer('account_manager_id')->nullable();
             $table->integer('user_id')->nullable();
             $table->text('job_sub_description')->nullable();
@@ -60,7 +61,7 @@ return new class extends Migration
             // $table->string('assigned_tech')->nullable();
             $table->string('notify_tech_assign')->nullable();
             $table->string('notes_for_tech')->nullable();
-            $table->string('schedule_at')->nullable();
+            $table->string('scheduled_at')->nullable();
             $table->string('completion_notes')->nullable(); 
             $table->string('image')->nullable();
             $table->string('document')->nullable();
