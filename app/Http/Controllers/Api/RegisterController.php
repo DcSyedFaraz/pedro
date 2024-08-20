1d270aaac3ceb4b3e580b06ae55cc38b98fcc9c2
Syed <dcsyedfaraz@gmail.com>
2024-08-20 19:45:36 +0500

    "Refactored RegisterController: reformatted code, removed unnecessary whitespace, and made minor syntax adjustments."

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -17,7 +17,7 @@ use Validator;
 use App\Mail\SendVerifyCode;
 use Mail;
 use Carbon\Carbon;
-use Twilio\Rest\Client; 
+use Twilio\Rest\Client;
 use Hash;
 use Image;
 use File;
@@ -30,164 +30,141 @@ class RegisterController extends BaseController
     {
         return User::where('email', $email)->first();
     }
-    
+
 
     public function register(Request $request)
     {
-		$validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string',
-            'email' => 'required|email|unique:users',			
-            'phone' => 'required',			
+            'email' => 'required|email|unique:users',
+            'phone' => 'required',
             'password' => 'required|min:8',
             'confirm_password' => 'required|same:password',
-			'photo' => 'image|mimes:jpeg,png,jpg,bmp,gif,svg|max:2048',
-        ]);      
-        if($validator->fails())
-        {
-		    return $this->sendError($validator->errors()->first());
+            'photo' => 'image|mimes:jpeg,png,jpg,bmp,gif,svg|max:2048',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors()->first());
         }
-		$fileName = null;
-        if($request->hasFile('photo')) 
-        {
+        $fileName = null;
+        if ($request->hasFile('photo')) {
             $file = request()->file('photo');
             $fileName = md5($file->getClientOriginalName() . time()) . $file->getClientOriginalExtension();
-            $file->move('uploads/user/profiles/', $fileName);  
-            $profile = asset('uploads/user/profiles/'.$fileName);
+            $file->move('uploads/user/profiles/', $fileName);
+            $profile = asset('uploads/user/profiles/' . $fileName);
         }
 
-        $input = $request->except(['confirm_password'],$request->all());
+        $input = $request->except(['confirm_password'], $request->all());
         $input['password'] = bcrypt($input['password']);
-        
-        $input['photo'] = '/uploads/user/profiles/'.$fileName;//$profile;
-		$input['email_verified_at'] = Carbon::now();
+
+        $input['photo'] = '/uploads/user/profiles/' . $fileName;//$profile;
+        $input['email_verified_at'] = Carbon::now();
         $user = User::create($input);
 
-        $token =  $user->createToken('dr-peter')->plainTextToken;
+        $token = $user->createToken('dr-peter')->plainTextToken;
         $users = $this->userinfo($request->email);
 
-		return response()->json(['success'=>true,'message'=>'User register successfully' ,'token'=>$token,'user_info'=>$users]);
+        return response()->json(['success' => true, 'message' => 'User register successfully', 'token' => $token, 'user_info' => $users]);
     }
 
     public function login(Request $request)
-    {   
-        if(!empty($request->email) || !empty($request->password))
-        {
+    {
+        if (!empty($request->email) || !empty($request->password)) {
             $validator = Validator::make($request->all(), [
                 'email' => 'required|email|exists:users',
-                'password' => 'required',        
-            ]);  
-            if($validator->fails())
-            {
-				return $this->sendError($validator->errors()->first());
+                'password' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError($validator->errors()->first());
             }
 
-            $user = User::firstWhere('email',$request->email);
-            
-            if(Auth::attempt(['email' => $request->email, 'password' => $request->password]))
-            { 
+            $user = User::firstWhere('email', $request->email);
+
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
                 $user = Auth::user();
-                $token =  $user->createToken('app_api')->plainTextToken; 
+                $token = $user->createToken('app_api')->plainTextToken;
                 $users = $this->userinfo($request->email);
-                return response()->json(['success'=>true,'message'=>'User Logged In successfully' ,'token'=>$token,'user_info'=>$users]);
-            } 
-            else
-            { 
+                $role = $user->roles->first()->name;
+                return response()->json(['success' => true, 'message' => 'User Logged In successfully', 'token' => $token, 'user_info' => $users, 'role' => $role,]);
+            } else {
                 return $this->sendError('Incorrect Password');
-            } 
-        }
-        else
-        {
-		    return $this->sendError('Email & Password are Required');
+            }
+        } else {
+            return $this->sendError('Email & Password are Required');
         }
     }
 
     public function me()
     {
-        $user = User::with(['child','goal','temporary_wallet','wallet','payments'])->where('id',Auth::user()->id)->first(); 
-        return response()->json(['success'=>true,'message'=>'User Fetch successfully','user_info'=>$user]);
+        $user = User::with(['child', 'goal', 'temporary_wallet', 'wallet', 'payments'])->where('id', Auth::user()->id)->first();
+        return response()->json(['success' => true, 'message' => 'User Fetch successfully', 'user_info' => $user]);
     }
     public function logout()
     {
-        if(Auth::check())
-        {
+        if (Auth::check()) {
             $user = Auth::user()->token();
             $user->revoke();
-            $success['success'] =true; 
+            $success['success'] = true;
             return $this->sendResponse($success, 'User Logout successfully.');
-        }
-        else
-        {
+        } else {
             return $this->sendError('No user in Session .');
         }
     }
     public function user(Request $request)
     {
-        if(Auth::check())
-        {
+        if (Auth::check()) {
             $success['user_info'] = $request->user();
             return $this->sendResponse($success, 'Current user successfully.');
-        }
-        else
-        {
+        } else {
             return $this->sendError('No user in Session .');
         }
     }
     public function verify(Request $request)
     {
-		$validator = Validator::make($request->all(),['email_code'=>'required']);
-        if($validator->fails())
-        {
-            return $this->sendError($validator->errors()->first());       
+        $validator = Validator::make($request->all(), ['email_code' => 'required']);
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors()->first());
         }
 
-        $user = User::firstWhere('email_code',$request->email_code);
-        if($user == null)
-        {
+        $user = User::firstWhere('email_code', $request->email_code);
+        if ($user == null) {
             return $this->sendError('Token Expire or Invalid');
-        }
-        else
-        {
-            $user->update(['email_verified_at'=>Carbon::now(),'email_code'=>null]);
-            $success['success'] =true; 
+        } else {
+            $user->update(['email_verified_at' => Carbon::now(), 'email_code' => null]);
+            $success['success'] = true;
             return $this->sendResponse($success, 'Email verified Successfully');
         }
     }
     public function change_password(Request $request)
     {
-        try
-        {
-                                          
-            $validator = Validator::make($request->all(),[
+        try {
+
+            $validator = Validator::make($request->all(), [
                 'current_password' => 'required',
                 'new_password' => 'required|same:confirm_password|min:8',
                 'confirm_password' => 'required',
             ]);
 
-            if($validator->fails())
-            {
-                return $this->sendError($validator->errors()->first());       
+            if ($validator->fails()) {
+                return $this->sendError($validator->errors()->first());
             }
             $user = Auth::user();
 
-            if (!Hash::check($request->current_password,$user->password)) 
-            {
+            if (!Hash::check($request->current_password, $user->password)) {
                 return $this->sendError('Current Password Not Matched');
             }
             $user->password = Hash::make($request->new_password);
             $user->save();
 
-            return response()->json(['success'=>true,'message'=>'Password Successfully Changed','user_info'=>$user]);
+            return response()->json(['success' => true, 'message' => 'Password Successfully Changed', 'user_info' => $user]);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
         }
-        catch(\Eception $e)
-        {
-            return $this->sendError($e->getMessage());    
-        }
-    }   
+    }
 
     public function noauth()
     {
-	    return $this->sendError('session destroyed , Login to continue!');
-	}
-	
+        return $this->sendError('session destroyed , Login to continue!');
+    }
+
 
 }
