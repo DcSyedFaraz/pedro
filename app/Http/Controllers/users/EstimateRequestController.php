b3fa76b1e61c9b9f8e599eb0d2d9e81b8cccad8c
Syed <dcsyedfaraz@gmail.com>
2024-11-19 21:40:57 +0500

    Remove unique validation rule from email field in EstimateRequestController store method.

--- app/Http/Controllers/users/EstimateRequestController.php
+++ app/Http/Controllers/users/EstimateRequestController.php
@@ -61,7 +61,7 @@ class EstimateRequestController extends Controller
                 'first_name' => 'required|string|max:255',
                 'last_name' => 'required|string|max:255',
                 'phone_number' => 'required|string|max:20',
-                'email' => 'required|email|unique:users|max:255',
+                'email' => 'required|email|max:255',
                 'street_address' => 'required|string|max:255',
                 'city' => 'required|string|max:255',
                 'state' => 'required|string|max:255',
