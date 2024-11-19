175f44a9fb42f3d3ae0486f339e7d2e9000c5766
Syed <dcsyedfaraz@gmail.com>
2024-11-19 21:33:49 +0500

    "Commented out entire up and down methods in create_inventories_table migration"

--- database/migrations/2025_05_31_121050_create_inventories_table.php
+++ database/migrations/2025_05_31_121050_create_inventories_table.php
@@ -11,36 +11,36 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('inventories', function (Blueprint $table) {
-            $table->id();
-            $table->string('vendor')->nullable();
-            $table->string('date')->nullable();
-            $table->string('paid_for')->nullable();
-            $table->string('paid')->nullable();
-            $table->string('receive')->nullable();
-            $table->string('product')->nullable();
-            $table->string('quanity')->nullable();
-            $table->string('unreceived')->nullable();
-            $table->string('unit_cost')->nullable();
-            $table->string('total')->nullable();
-            $table->string('sub_total')->nullable();
-            $table->string('discount')->nullable();
-            $table->string('tax_paid')->nullable();
-            $table->string('ship_cost')->nullable();
-            $table->string('grand_total')->nullable();
-            $table->timestamps();
-        });
-    }
+    // public function up()
+    // {
+    //     Schema::create('inventories', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->string('vendor')->nullable();
+    //         $table->string('date')->nullable();
+    //         $table->string('paid_for')->nullable();
+    //         $table->string('paid')->nullable();
+    //         $table->string('receive')->nullable();
+    //         $table->string('product')->nullable();
+    //         $table->string('quanity')->nullable();
+    //         $table->string('unreceived')->nullable();
+    //         $table->string('unit_cost')->nullable();
+    //         $table->string('total')->nullable();
+    //         $table->string('sub_total')->nullable();
+    //         $table->string('discount')->nullable();
+    //         $table->string('tax_paid')->nullable();
+    //         $table->string('ship_cost')->nullable();
+    //         $table->string('grand_total')->nullable();
+    //         $table->timestamps();
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('inventories');
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function down()
+    // {
+    //     Schema::dropIfExists('inventories');
+    // }
 };
